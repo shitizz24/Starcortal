b4e042d1d501270e08103b20038b2336ffc3e5f2
Shitiz Khanna <shitizmvn@gmail.com>
2019-06-17 14:28:58 +0530

    Initial version

---
+++ app.js
@@ -0,0 +1,44 @@
+var express    =   require("express"),
+    ejs        =   require('ejs'),
+    bodyparser =   require("body-parser"),
+    request    =   require('request'),
+    app        =   express();
+app.use(bodyparser.urlencoded({extended : true}))
+
+ request('http://www.omdbapi.com/?apikey=775e324f&s=Harry+Potter',function(error,response,body){
+   if(error){
+     console.log('error');
+     console.log(error);
+   }
+   if(!error && response.statusCode==200){
+    var data=JSON.parse(body);
+     console.log(data['Search']);
+   }
+ })
+ app.get('/',function(req,res){
+   res.render('1.ejs');
+ })
+app.get("/api",function(req,res) {
+  console.log(req.query);
+  var type=req.query.type;
+  if(type=="movies"){
+  var search=req.query.url;
+  var rest="http://www.omdbapi.com/?apikey=775e324f&s="+search}
+  console.log(rest);
+  request(rest,function(error,response,body){
+    if(error){
+      console.log('Error');
+      console.log(error);
+    }
+    if(!error && response.statusCode==200){
+      var data=JSON.parse(body);
+      res.render('2.ejs',{data : data})
+    }
+  })
+})
+app.get('/test',function(req,res){
+  res.send("Hi there!")
+})
+app.listen(3000,function(req,res){
+  console.log('Server Started at port 4000!');
+})
